ff7abfd26fc3c4d3068f3de30af2c04247c101c8
ColaCW <2629080132@qq.com>
2019-03-07 15:59:41 +0800

    blog

--- src/components/BlogDetail.vue
+++ src/components/BlogDetail.vue
@@ -72,6 +72,8 @@
 </template>
 <style scoped>
   @import '../../static/css/blogDetail.css';
+  @import '../../static/css/style.css';
+  @import '../../static/css/foundation.min.css';
 </style>
 <script>
   import { Web } from "../../static/js/web.js";
@@ -81,7 +83,7 @@
         id:"",
         isGood:false,
         previewBlog:null,
-        blogs:[{"name":11111},{"name":22222},{"name":33333},{"name":44444},{"name":55555},{"name":66666},{"name":77777},{"name":88888},{"name":9999},{"name":10000}],
+        blogs:[],
         goodBlogs:[],
         viewBlogs:[],
         blogTags:[]
