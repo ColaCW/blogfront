9a89435efde5d8c347c9f7d6b79e9e48ff206ebc
ColaCW <2629080132@qq.com>
2019-03-14 13:35:57 +0800

    跨域发送请求session改变

--- static/js/web.js
+++ static/js/web.js
@@ -25,6 +25,9 @@ var Web = {
       type: 'GET',
       contentType: "application/json; charset=utf-8",
       url: url,
+      xhrFields: {
+        withCredentials: true
+      },
       data: data2,
       dataType: "json",
       success: success
@@ -36,6 +39,9 @@ var Web = {
       type: 'POST',
       contentType: "application/json; charset=utf-8",
       url: url,
+      xhrFields: {
+        withCredentials: true
+      },
       data: JSON.stringify(data),
       dataType: "json",
       success: success
